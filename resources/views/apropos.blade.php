f7b44d8ebd78091165a166070435abfd21897b66
Djakpo GADO <djakpo.gado20@gmail.com>
2024-09-03 23:25:50 +0000

    Fin de mofication de A propos

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -56,17 +56,17 @@
     <!-- About End -->
 
     <!-- Team Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s" id="e">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-uppercase">DSI Niger</h5>
-                <h1 class="mb-0">LES MEMBRES DU BEAUREAU DU CLUB</h1>
+                <h5 class="fw-bold text-uppercase">Notre équipe</h5>
+                <h1 class="mb-0">Les Membres du Bureau Exécutif</h1>
             </div>
             <div class="row g-5">
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="{{asset('img/teams-1.jpg')}}" alt="">
+                            <img class="img-fluid w-100" src="{{asset('img/sg.jpeg')}}" alt="">
                             <div class="team-social">
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
@@ -75,15 +75,15 @@
                             </div>
                         </div>
                         <div class="text-center py-4">
-                            <h4 class="" style="color: #E05206">Full Name</h4>
-                            <p class="text-uppercase m-0">Designation</p>
+                            <h4 class="" style="color: #E05206">WADATAOU MAMAN Lawal</h4>
+                            <p class="text-uppercase m-0">Président</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
+                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="{{asset('img/teams-2.jpg')}}" alt="">
+                            <img class="img-fluid w-100" src="{{asset('img/sg.jpeg')}}" alt="">
                             <div class="team-social">
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
@@ -92,15 +92,15 @@
                             </div>
                         </div>
                         <div class="text-center py-4">
-                            <h4 class=""style="color: #E05206">Full Name</h4>
-                            <p class="text-uppercase m-0">Designation</p>
+                            <h4 class="" style="color: #E05206">WADATAOU MAMAN Lawal</h4>
+                            <p class="text-uppercase m-0">Sécrétaire Général</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="{{asset('img/teams-3.jpg')}}" alt="">
+                            <img class="img-fluid w-100" src="{{asset('img/sg.jpeg')}}" alt="">
                             <div class="team-social">
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
@@ -109,16 +109,31 @@
                             </div>
                         </div>
                         <div class="text-center py-4">
-                            <h4 class=""style="color: #E05206">Full Name</h4>
-                            <p class="text-uppercase m-0">Designation</p>
+                            <h4 class="" style="color: #E05206">WADATAOU MAMAN Lawal</h4>
+                            <p class="text-uppercase m-0">Trésorier Général</p>
                         </div>
-                        <a href="{{route('equipe')}}">Voir l'équipe</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
+                    <div class="team-item bg-light rounded overflow-hidden">
+                        <div class="team-img position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="{{asset('img/sg.jpeg')}}" alt="">
+                            <div class="team-social">
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                            </div>
+                        </div>
+                        <div class="text-center py-4">
+                            <h4 class="" style="color: #E05206">WADATAOU MAMAN Lawal</h4>
+                            <p class="text-uppercase m-0">Secrétaire à la Communication, et aux Relations Extérieures</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Team End -->
 
     <!-- Vendor Start -->
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
