c2c223be48736d75fda1b31dabd6653be8dcddb8
Djakpo GADO <djakpo.gado20@gmail.com>
2024-09-04 00:51:10 +0000

    Modification de Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -39,8 +39,8 @@
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Contact Us</h5>
-                <h1 class="mb-0">If You Have Any Query, Feel Free To Contact Us</h1>
+                <h5 class="fw-bold text-primary text-uppercase">Contactez-nous</h5>
+                <h1 class="mb-0">Soyez libre de nous contacter si vous avez besoins d'informations</h1>
             </div>
             <div class="row g-5 mb-5">
                 <div class="col-lg-4">
@@ -49,8 +49,8 @@
                             <i class="fa fa-phone-alt text-white"></i>
                         </div>
                         <div class="ps-4">
-                            <h5 class="mb-2">Call to ask any question</h5>
-                            <h4 class="text-primary mb-0">+012 345 6789</h4>
+                            <h5 class="mb-2">Nous sommes joignable au</h5>
+                            <h4 class="text-primary mb-0">+227 96 53 53 93</h4>
                         </div>
                     </div>
                 </div>
@@ -60,8 +60,8 @@
                             <i class="fa fa-envelope-open text-white"></i>
                         </div>
                         <div class="ps-4">
-                            <h5 class="mb-2">Email to get free quote</h5>
-                            <h4 class="text-primary mb-0">info@example.com</h4>
+                            <h5 class="mb-2">Contactez-nous par email</h5>
+                            <h4 class="text-primary mb-0">info@dsiniger.org</h4>
                         </div>
                     </div>
                 </div>
@@ -71,8 +71,8 @@
                             <i class="fa fa-map-marker-alt text-white"></i>
                         </div>
                         <div class="ps-4">
-                            <h5 class="mb-2">Visit our office</h5>
-                            <h4 class="text-primary mb-0">123 Street, NY, USA</h4>
+                            <h5 class="mb-2">Passez dans nos locaux</h5>
+                            <h4 class="text-primary mb-0">BP: 123, Niamey, Niger</h4>
                         </div>
                     </div>
                 </div>
@@ -82,19 +82,19 @@
                     <form>
                         <div class="row g-3">
                             <div class="col-md-6">
-                                <input type="text" class="form-control border-0 bg-light px-4" placeholder="Your Name" style="height: 55px;">
+                                <input type="text" class="form-control border-0 bg-light px-4" placeholder="Nom" style="height: 55px;">
                             </div>
                             <div class="col-md-6">
-                                <input type="email" class="form-control border-0 bg-light px-4" placeholder="Your Email" style="height: 55px;">
+                                <input type="email" class="form-control border-0 bg-light px-4" placeholder="Email" style="height: 55px;">
                             </div>
                             <div class="col-12">
-                                <input type="text" class="form-control border-0 bg-light px-4" placeholder="Subject" style="height: 55px;">
+                                <input type="text" class="form-control border-0 bg-light px-4" placeholder="Objet" style="height: 55px;">
                             </div>
                             <div class="col-12">
                                 <textarea class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message"></textarea>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                                <button class="btn btn-primary w-100 py-3" type="submit">Envoyer</button>
                             </div>
                         </div>
                     </form>
